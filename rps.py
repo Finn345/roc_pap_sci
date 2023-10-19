16b5ce308b67548841f37b61300816cb03faea02
Kieran <85902219+Finn345@users.noreply.github.com>
2023-10-19 13:11:00 -0500

    Add files via upload

--- rps.py
+++ rps.py
@@ -1,19 +1,25 @@
 import random
 
 choices = ['rock', 'paper', 'scissors']
-player_choice = input("Choose Rock, Paper, or Scissors: ").lower()
 
-computer_choice = random.choice(choices)
+while True:
+    player_choice = input("Choose: rock, paper, scissors, or 'quit' to exit: ").lower()
 
-print('Player Chooses: ', player_choice)
-print('Computer Chooses: ', computer_choice)
+    if player_choice == 'quit':
+        break
+    if player_choice in choices:
+        computer_choice = random.choice(choices)
 
-if player_choice in choices:
-    if player_choice == computer_choice:
-        print("Tie!")
-    elif (player_choice == "rock" and computer_choice == "scissors") or \
-         (player_choice == "scissors" and computer_choice == "paper") or \
-         (player_choice == "paper" and computer_choice == "rock"):
-        print("You Win!")
+        print('Player Chooses: ', player_choice)
+        print('Computer Chooses: ', computer_choice)
+
+        if player_choice == computer_choice:
+            print("Tie!")
+        elif (player_choice == "rock" and computer_choice == "scissors") or \
+             (player_choice == "scissors" and computer_choice == "paper") or \
+             (player_choice == "paper" and computer_choice == "rock"):
+            print("You Win!")
+        else:
+            print('You Lose! :(')
     else:
-        print('You Lose! :(')
+        print('Invalid choice. Please choose rock, paper, scissors, or "quit".')
