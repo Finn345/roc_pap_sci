40ed6b432ad53782a8b4a8c4aec4098d0934555e
Kieran <85902219+Finn345@users.noreply.github.com>
2023-10-19 13:03:08 -0500

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,19 @@
+import random
+
+choices = ['rock', 'paper', 'scissors']
+player_choice = input("Choose Rock, Paper, or Scissors: ").lower()
+
+computer_choice = random.choice(choices)
+
+print('Player Chooses: ', player_choice)
+print('Computer Chooses: ', computer_choice)
+
+if player_choice in choices:
+    if player_choice == computer_choice:
+        print("Tie!")
+    elif (player_choice == "rock" and computer_choice == "scissors") or \
+         (player_choice == "scissors" and computer_choice == "paper") or \
+         (player_choice == "paper" and computer_choice == "rock"):
+        print("You Win!")
+    else:
+        print('You Lose! :(')
